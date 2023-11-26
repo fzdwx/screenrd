ed4df48c804a8eb0aa069ffb42511702c9f85e7e
fzdwx <likelovec@gmail.com>
2023-11-26 13:17:25 +0800

    chore: fmt

--- corp.go
+++ corp.go
@@ -15,15 +15,21 @@ type Corp struct {
 }
 
 func (c *Corp) isEmpty() bool {
-	return c.X == 0 && c.Y == 0 && c.Width == 0 && c.Height == 0
+	return c.X == 0 &&
+		c.Y == 0 &&
+		c.Width == 0 &&
+		c.Height == 0
 }
 
 func (c *Corp) Crop(tempFile *os.File, outFilename string) error {
 	args := []string{"-i", tempFile.Name()}
 	if c.isEmpty() == false {
-		args = append(args, "-filter:v", fmt.Sprintf("crop=%d:%d:%d:%d", int(c.Width), int(c.Height), int(c.X), int(c.Y)))
+		cropArgs := fmt.Sprintf("crop=%d:%d:%d:%d", int(c.Width), int(c.Height), int(c.X), int(c.Y))
+		args = append(args, "-filter:v", cropArgs)
 	}
+
 	args = append(args, outFilename)
+
 	return exec.Command("ffmpeg", args...).Run()
 }
 
